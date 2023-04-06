b62a5c8649dc23f4a41005fb2403416f74415e02
chrisgom113 <cgomez4@uci.edu>
2023-04-06 11:26:46 -0700

    signup view, signup route, signuphandler added

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -40,4 +40,13 @@ router.get('/posts/:id', async (req, res) => {
   }
 });
 
+router.get("/signup", (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect("/");
+        return;
+    }
+
+    res.render("signup");
+});
+
 module.exports = router;
