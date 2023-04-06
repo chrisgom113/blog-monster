8091c9c8c418332802c5d4014fb404d7dce9c567
chrisgom113 <cgomez4@uci.edu>
2023-04-06 10:52:37 -0700

    single post route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -10,7 +10,31 @@ router.get('/', async (req, res) => {
 
     const posts = postData.map((post) => post.get({ plain: true }));
 
-    res.render('public-view', { posts });
+    res.render('all', { posts });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get('/posts/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        User,
+        {
+          model: Comment,
+          include: [User],
+        },
+      ],
+    });
+
+    if (postData) {
+      const post = postData.get({ plain: true });
+
+      res.render('single', { post });
+    } else {
+      res.status(404).json({ message: 'This post does not exist' });
+    }
   } catch (error) {
     res.status(500).json(error);
   }
