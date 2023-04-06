16a7ee193f11b0328c5bdf7a9c3a451d8c916f3a
chrisgom113 <cgomez4@uci.edu>
2023-04-06 09:35:40 -0700

    setup server, findAll homeroute, index controller, helpers

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const { Post, Comment, User } = require('../models/');
+
+//View all posts
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [User],
+    });
+
+    const posts = psotData.map((post) => post.get({ plain: true }));
+
+    res.render('public-view', { posts });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
